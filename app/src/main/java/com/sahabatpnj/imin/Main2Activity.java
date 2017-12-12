85953041a51fa29ee6ed07b7a3c83b1fdb276005
zmkhtr <azammukhtar3@gmail.com>
2017-12-12 22:32:35 +0700

    asdfsa

--- app/src/main/java/com/sahabatpnj/imin/Main2Activity.java
+++ app/src/main/java/com/sahabatpnj/imin/Main2Activity.java
@@ -3,10 +3,11 @@ package com.sahabatpnj.imin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
